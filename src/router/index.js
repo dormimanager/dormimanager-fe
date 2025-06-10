ae213d4e63bd0e77a438ae96e0bce7e7c639e382
Jo <gho0612@gmail.com>
2025-06-10 15:35:18 +0900

    수정정

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import HelloWorld from "@/components/HelloWorld.vue";
+import MainPage from '@/components/MainPage.vue';
 
 const routes = [
     {
@@ -8,6 +9,7 @@ const routes = [
         name: 'Home',
         component: HelloWorld
     },
+    { path: '/main', name: 'MainPage', component: MainPage },
     // 필요한 경로 추가
 ]
 
